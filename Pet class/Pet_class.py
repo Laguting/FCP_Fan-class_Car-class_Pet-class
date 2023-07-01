ccc16765cf2a89e779fa02bf12dd0fc62b5366d7
Laguting <lagutingmariconjaneg@gmail.com>
2023-07-01 18:40:44 +0800

    tried my initial idea of how the pet class' attribute will look like

--- Pet class/Pet_class.py
+++ Pet class/Pet_class.py
@@ -7,8 +7,20 @@ class Pet():
         self.__age = age
 # Methods
     # This method returns the value of the _ _ name field.
+    def get_name(self):
+        return self.__name
     # This method returns the value of the _ _animal_type field.
+    def get_animaltype(self):
+        return self.__animal_type
     # This method returns the value of the _ _age field. 
+    def get_age(self):
+        return self.__age
     # This method assigns a value to the _ _name field.
+    def set_name(self):
+        self.__name = ""
     # This method assigns a value to the _ _animal_type field.
+    def set_animaltype(self):
+        self.__animal_type = ""
     # This method assigns a value to the _ _age field.
+    def set_age(self):
+        self.__age = 0
