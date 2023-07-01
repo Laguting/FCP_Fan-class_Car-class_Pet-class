ce8573bad78b368f6fc248410adee1e794c95015
Laguting <lagutingmariconjaneg@gmail.com>
2023-07-01 18:09:02 +0800

    Was able to get the car accelet=ration and car brake by modifying the car attributes

--- Car class/C_mainclass.py
+++ Car class/C_mainclass.py
@@ -10,5 +10,16 @@ class testCar():
     print(de_1.show_welcome()) # Welcome the user
     print(de_1.loading_bar()) # show the initiation of the fan
 # Test Car
+    car_obj = Car (2021,"A3THER", 0)
+    #  After each call to the accelerate method, get the current speed of the car and display it.
+    print("\33[31m\33[1m                                                                      --------CAR ACCELERATION---------- \33[0m")
+    for i in range(5):
+        car_obj.accelerate_car()
+        print("\33[32m                                                                           Current Speed: ", car_obj.get_speed())
+    # Then call the brake method five times. After each call to the brake method, get the current speed of the car and display it.
+    print("\33[31m\33[1m                                                                      ------------CAR BRAKE------------ \33[0m")
+    for i in range (5):
+        car_obj.brake_car()
+        print("\33[32m                                                                           Current Speed: ", car_obj.get_speed())
 # Close the program
     print(de_1.closing_fan())
--- Car class/Car_class.py
+++ Car class/Car_class.py
@@ -1,17 +1,19 @@
 # Setup class Car
 class Car():
     # Car's attributes
-    def __init__(self, year_model, make, speed = 0):
+    def __init__(self, year_model, make, speed):
         self.__year_model = year_model
         self.__make = make
         self.__speed = speed
     # Car's methods
     def accelerate_car(self): # The accelerate method should add 5 to the speed data attribute each time it is called.
-        return self.__speed + 5
+        self.__speed += 5
     def brake_car(self): # The brake method should subtract 5 from the speed data attribute each time it is called.
-        return self.__speed - 5
+        self.__speed -= 5
     def get_speed(self): # The get_speed method should return the current speed.
         return self.__speed
+    def set_speed(self):
+        self.get_speed = 0
     def get_yearmodel(self):
         return self.__year_model
     def get_make(self):
