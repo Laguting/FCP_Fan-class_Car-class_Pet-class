da4318ffcc18490e0bfc68086f306b6a170dee95
Laguting <lagutingmariconjaneg@gmail.com>
2023-07-01 18:32:34 +0800

    laid out the pseudocodes

---
+++ Pet class/Pet_class.py
@@ -0,0 +1,14 @@
+# Setup Pet class
+class Pet():
+    # Pet's attributes
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+# Methods
+    # This method returns the value of the _ _ name field.
+    # This method returns the value of the _ _animal_type field.
+    # This method returns the value of the _ _age field. 
+    # This method assigns a value to the _ _name field.
+    # This method assigns a value to the _ _animal_type field.
+    # This method assigns a value to the _ _age field.
