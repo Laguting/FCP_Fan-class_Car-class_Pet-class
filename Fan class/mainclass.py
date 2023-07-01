81ba254d196e853efebc75bb63b88527347d6faf
Laguting <lagutingmariconjaneg@gmail.com>
2023-07-01 14:04:55 +0800

    Finished the fan attributes

---
+++ Fan class/mainclass.py
@@ -0,0 +1,8 @@
+# Laguting, Maricon Jane G.
+# BSCpE 1-4
+# Task: follow the instructions for test fan exercise in module 6: encapssulation and abstraction
+from Fan_class import Fan
+from Designs import designs
+class Testfan():
+    de_1 = designs("","","")
+    print(de_1.get_brand()) # Display of the brand
